5ff7ad80813ce3797a9e0ea547304dcb4311cb74
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-05-25 12:25:56 +0700

    Update proxy.js

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/proxy.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/proxy.js
@@ -26,7 +26,7 @@ return view.extend({
 
         m = new form.Map('nikki');
 
-        s = m.section(form.NamedSection, 'proxy', 'proxy', _('Proxy Config'));
+        s = m.section(form.NamedSection, 'proxy', 'proxy', _('<p><strong>❖ 𝕻𝖗𝖔𝖝𝖞 𝕮𝖔𝖓𝖋𝖎𝖌</strong></p>'));
 
         s.tab('proxy', _('Proxy Config'));
 
